915cce1011aafcd7fc53eb68d29353ef19294bca
csf0429 <csf0429@gmail.com>
2017-08-16 18:33:31 +0800

    62

--- Array/63. Unique Paths II .cpp
+++ Array/63. Unique Paths II .cpp
@@ -2,6 +2,10 @@
 // Created by 蔡少凡 on 8/15/17.
 //
 
+//
+// Created by 蔡少凡 on 8/15/17.
+//
+
 #include <iostream>
 using namespace std;
 
@@ -17,18 +21,23 @@ public:
             return 0;
 
         vector<vector <int>> map (m, vector<int> (n,0));
+        int rflag = 0 ; int cflag = 0;
         for(int i = 0 ; i < m ; i ++){
-            if(obstacleGrid[i][n-1] == 1)
-                map[i][n-1] = -1;
-            else
+            if(obstacleGrid[i][n-1] == 0 && rflag ==0){
                 map[i][n-1] = 1;
+                rflag = 1;
+            }
+            else
+                map[i][n-1] = 0;
         }
 
         for(int j = 0 ; j < n ; j++){
-            if(obstacleGrid[m-1][j] == 1)
-                map[m-1][j] = 0;
-            else
+            if(obstacleGrid[m-1][j] == 0 && cflag == 0){
                 map[m-1][j] = 1;
+                cflag = 1;
+            }
+            else
+                map[m-1][j] = 0;
         }
 
         if(m != 1 && n != 1){
@@ -40,8 +49,6 @@ public:
                         } else{
                             map[i][j] = map[i+1][j] + map[i][j+1];
                         }
-
-
                     }else{
                         map[i][j] = 0;
                     }
@@ -49,15 +56,14 @@ public:
             }
         }
         return map[0][0];
-
     }
 };
 
-
 int main(){
 //    cout << Solution().uniquePaths(1,2) << endl;
+    cout << "hello" << endl;
     vector<vector <int>> matrix = {{0,0},{1,1},{0,0}};
-    Solution().uniquePathsWithObstacles(matrix) << endl;
+    cout << Solution().uniquePathsWithObstacles(matrix) << endl;
     return 0;
 
 }
